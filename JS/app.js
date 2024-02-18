2eb79a1fbc1e2ed5cfb1b22f0561bcae5128f243
Partho-Mondal <146032031+Partho-Mondal@users.noreply.github.com>
2024-02-18 13:21:40 +0530

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,9 @@
 // Game constants and variables
 let inputDirection = {x : 0, y : 0};
-const foodSound = new Audio("/Snake Game/Assets/music/food.mp3");
-const gameOverSound = new Audio("/Snake Game/Assets/music/gameover.mp3");
-const moveSound = new Audio("/Snake Game/Assets/music/move.mp3");
-const musicSound = new Audio("/Snake Game/Assets/music/music.mp3");
+const foodSound = new Audio("/Assets/music/food.mp3");
+const gameOverSound = new Audio("/Assets/music/gameover.mp3");
+const moveSound = new Audio("/Assets/music/move.mp3");
+const musicSound = new Audio("/Assets/music/music.mp3");
 
 let speed = 10;
 let score = 0;
